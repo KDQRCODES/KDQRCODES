d1a969a36f1fd79ceee75c2164035af7298d3d79
KDQRCODES <kdqrcode@gmail.com>
2025-07-30 15:50:40 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAiA6yOsbv6gKtTdjeKXWZb3wutx0Ipv4c",
+  authDomain: "kd-qr-codes-checkin-eventos.firebaseapp.com",
+  projectId: "kd-qr-codes-checkin-eventos",
+  storageBucket: "kd-qr-codes-checkin-eventos.appspot.com",
+  messagingSenderId: "9325387244",
+  appId: "1:9325387244:web:7c0e2c03ede84d860d74c9"
+};
+
+firebase.initializeApp(firebaseConfig);
 // Firebase config com suas credenciais
